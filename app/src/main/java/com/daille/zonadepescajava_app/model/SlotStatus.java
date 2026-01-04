ee9c4911951d8212b2c54d35e772abe14560809c
Matias Daille <matiasdaille@gmail.com>
2026-01-04 03:10:20 -0300

    Implementar habilidades faltantes y recuperar dados tras captura

--- app/src/main/java/com/daille/zonadepescajava_app/model/SlotStatus.java
+++ app/src/main/java/com/daille/zonadepescajava_app/model/SlotStatus.java
@@ -4,4 +4,5 @@ public class SlotStatus {
     public boolean protectedOnce = false;
     public boolean calamarForcedFaceDown = false;
     public int sumConditionShift = 0;
+    public java.util.List<Card> attachedRemoras = new java.util.ArrayList<>();
 }
