6f89423b70855baa105e0d712f33389cb8996b46
Matias Daille <matiasdaille@gmail.com>
2026-01-18 15:38:03 -0300

    0.077 actulizacion de precios

--- app/src/main/java/com/daille/zonadepescajava_app/model/ShopPrices.java
+++ app/src/main/java/com/daille/zonadepescajava_app/model/ShopPrices.java
@@ -1,18 +1,18 @@
 package com.daille.zonadepescajava_app.model;
 
 public final class ShopPrices {
-    public static final int D4_COST = 200;
-    public static final int D6_COST = 400;
-    public static final int D8_COST = 600;
-    public static final int D10_COST = 800;
-    public static final int D12_COST = 1000;
-    public static final int D20_COST = 2000;
+    public static final int D4_COST = 100;
+    public static final int D6_COST = 150;
+    public static final int D8_COST = 200;
+    public static final int D10_COST = 250;
+    public static final int D12_COST = 300;
+    public static final int D20_COST = 500;
 
-    public static final int PACK_RANDOM_COST = 2000;
-    public static final int PACK_CRUSTACEO_COST = 2500;
-    public static final int PACK_SMALL_FISH_COST = 2500;
-    public static final int PACK_BIG_FISH_COST = 2500;
-    public static final int PACK_OBJECT_COST = 2500;
+    public static final int PACK_RANDOM_COST = 200;
+    public static final int PACK_CRUSTACEO_COST = 220;
+    public static final int PACK_SMALL_FISH_COST = 220;
+    public static final int PACK_BIG_FISH_COST = 220;
+    public static final int PACK_OBJECT_COST = 220;
 
     private ShopPrices() {
     }
@@ -20,13 +20,13 @@ public final class ShopPrices {
     public static int getDiceCapacityUpgradeCost(int targetCapacity) {
         switch (targetCapacity) {
             case 7:
-                return 300;
+                return 100;
             case 8:
-                return 500;
+                return 300;
             case 9:
-                return 1000;
+                return 500;
             case 10:
-                return 2000;
+                return 1000;
             default:
                 return Integer.MAX_VALUE;
         }
