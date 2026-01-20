563c97643150648bbe6fe549b5d5c6c518c3428d
Matias Daille <matiasdaille@gmail.com>
2026-01-20 10:45:03 -0300

    actualizacion de precios

--- app/src/main/java/com/daille/zonadepescajava_app/model/ShopPrices.java
+++ app/src/main/java/com/daille/zonadepescajava_app/model/ShopPrices.java
@@ -1,18 +1,18 @@
 package com.daille.zonadepescajava_app.model;
 
 public final class ShopPrices {
-    public static final int D4_COST = 100;
-    public static final int D6_COST = 150;
-    public static final int D8_COST = 200;
-    public static final int D10_COST = 250;
-    public static final int D12_COST = 300;
+    public static final int D4_COST = 50;
+    public static final int D6_COST = 100;
+    public static final int D8_COST = 150;
+    public static final int D10_COST = 200;
+    public static final int D12_COST = 250;
     public static final int D20_COST = 500;
 
-    public static final int PACK_RANDOM_COST = 200;
-    public static final int PACK_CRUSTACEO_COST = 220;
-    public static final int PACK_SMALL_FISH_COST = 220;
-    public static final int PACK_BIG_FISH_COST = 220;
-    public static final int PACK_OBJECT_COST = 220;
+    public static final int PACK_RANDOM_COST = 100;
+    public static final int PACK_CRUSTACEO_COST = 120;
+    public static final int PACK_SMALL_FISH_COST = 120;
+    public static final int PACK_BIG_FISH_COST = 120;
+    public static final int PACK_OBJECT_COST = 120;
 
     private ShopPrices() {
     }
