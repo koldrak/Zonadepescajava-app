18b39cd200b2e7512b72c7169e82b38ce611e2de
Matias Daille <matiasdaille@gmail.com>
2026-01-25 00:03:16 -0300

    1.3 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,9 +10,9 @@ android {
         applicationId = "com.daille.zonadepescajava_app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.2"
-
+        versionCode = 3
+        versionName = "1.3"
+//correccion de errores con ballena azul
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
